ec5a7519a24588051bd4da9c695098ff349b6cd3
Papa-Sean <smsugrue@gmail.com>
2024-08-01 11:23:38 -0400

    Initial frontend setup moving on to backend

--- client/src/pages/DashboardLayout.jsx
+++ client/src/pages/DashboardLayout.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useContext, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/Dashboard';
 import { BigSidebar, Navbar, SmallSidebar } from '../components';
+import { checkDefaultTheme } from '../App';
 
 const DashboardContext = createContext();
 
@@ -9,10 +10,13 @@ const DashboardLayout = () => {
   // User temp
   const user = { name: 'Papa' };
   const [showSidebar, setShowSidebar] = useState(false);
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(checkDefaultTheme());
 
   const toggleDarkTheme = () => {
-    console.log('toggle dark theme');
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    document.body.classList.toggle('dark-theme', newDarkTheme);
+    localStorage.setItem('darkTheme', newDarkTheme);
   };
 
   const toggleSidebar = () => {
